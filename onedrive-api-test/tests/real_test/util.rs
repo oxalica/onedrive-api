8c650cbc6eb7a8e17b1c402104fa19d9626db191
oxalica <oxalicc@pm.me>
2024-02-18 22:31:15 -0500

    Remove `lazy_static` dependency

--- onedrive-api-test/tests/real_test/util.rs
+++ onedrive-api-test/tests/real_test/util.rs
@@ -1,4 +1,3 @@
-use lazy_static::lazy_static;
 use onedrive_api::*;
 use serde::Deserialize;
 
@@ -10,51 +9,38 @@ struct Env {
     refresh_token: String,
 }
 
-async fn login() -> String {
-    let env: Env = envy::prefixed("ONEDRIVE_API_TEST_").from_env().unwrap();
-
-    let auth = Auth::new(
-        env.client_id,
-        Permission::new_read().write(true).offline_access(true),
-        env.redirect_uri,
-    );
-
-    auth.login_with_refresh_token(&env.refresh_token, env.client_secret.as_deref())
-        .await
-        .expect("Login failed")
-        .access_token
-}
-
-lazy_static! {
-    pub static ref TOKEN: tokio::sync::Mutex<Option<String>> = tokio::sync::Mutex::default();
-}
+pub static TOKEN: tokio::sync::OnceCell<String> = tokio::sync::OnceCell::const_new();
 
 pub async fn get_logined_onedrive() -> OneDrive {
-    let mut guard = TOKEN.lock().await;
-    let token = match &*guard {
-        Some(token) => token.clone(),
-        None => {
-            let token = login().await;
-            *guard = Some(token.clone());
-            token
-        }
-    };
-    OneDrive::new(token, DriveLocation::me())
+    let token = TOKEN
+        .get_or_init(|| async {
+            let env: Env = envy::prefixed("ONEDRIVE_API_TEST_").from_env().unwrap();
+            let auth = Auth::new(
+                env.client_id,
+                Permission::new_read().write(true).offline_access(true),
+                env.redirect_uri,
+            );
+            auth.login_with_refresh_token(&env.refresh_token, env.client_secret.as_deref())
+                .await
+                .expect("Login failed")
+                .access_token
+        })
+        .await;
+    OneDrive::new(token.clone(), DriveLocation::me())
 }
 
 pub fn gen_filename() -> &'static FileName {
     use std::sync::atomic::{AtomicU64, Ordering};
+    use std::sync::OnceLock;
+
+    use rand::{rngs::StdRng, Rng, SeedableRng};
 
     // Randomly initialized counter.
-    lazy_static! {
-        static ref COUNTER: AtomicU64 = {
-            use rand::{rngs::StdRng, Rng, SeedableRng};
-            // Avoid overflow to keep it monotonic.
-            AtomicU64::new(u64::from(StdRng::from_entropy().gen::<u32>()))
-        };
-    }
-
-    let id = COUNTER.fetch_add(1, Ordering::SeqCst);
+    static COUNTER: OnceLock<AtomicU64> = OnceLock::new();
+    let id = COUNTER
+        // Avoid overflow to keep it monotonic.
+        .get_or_init(|| AtomicU64::new(u64::from(StdRng::from_entropy().gen::<u32>())))
+        .fetch_add(1, Ordering::Relaxed);
     let s = Box::leak(format!("$onedrive_api_tests.{id}").into_boxed_str());
     FileName::new(s).unwrap()
 }
